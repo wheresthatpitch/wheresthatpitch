0c38791e7a15099edc6d055ee3f2f0740f1689af
Mike Griffin <github@mikegriffin.ie>
2022-04-03 09:31:00 +0100

    Start adding some tests for the clubs admin page
    I added pagination to the view page too, it was supposed to be there
    already, thank goodness for tests :)

--- test/controllers/admin/clubs_controller_test.rb
+++ test/controllers/admin/clubs_controller_test.rb
@@ -3,6 +3,46 @@ require "test_helper"
 class Admin::ClubsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
+  test "you have to be logged in to view these pages" do
+    get admin_clubs_url
+    assert_response :redirect
+
+    assert_redirected_to new_user_session_url
+  end
+
+  test "logged in users can view the index page with no clubs" do
+    user = create(:user)
+
+    sign_in user
+
+    get admin_clubs_url
+    assert_response :success
+    assert_select "p", "There are no clubs yet"
+  end
+
+  test "logged in users will get a paginated view of many clubs" do
+    user = create(:user)
+    create_list(:club, 25)
+
+    sign_in user
+
+    get admin_clubs_url
+    assert_response :success
+
+    # pagination is set to 20 items, so we should have that many rows in the table
+    assert_select "table" do
+      assert_select "tbody" do
+        assert_select "tr", 20
+      end
+    end
+
+    # pagination is included at the bottom of the page, and there should be a link to page 2
+    assert_select "nav"
+    assert_select "span" do
+      assert_select "a", "2"
+    end
+  end
+
   test "should get new" do
     user = create(:user)
 
