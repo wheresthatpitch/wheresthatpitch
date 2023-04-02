5e4332360ac83aca96f7df36732bd27f7fce4b2b
Mike Griffin <mgriffin@github.com>
2023-04-02 23:09:10 +0100

    Comment out the rbenv prefix

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ append :linked_dirs, ".bundle"
 # set :ssh_options, verify_host_key: :secure
 
 set :rbenv_type, :user
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 set :rbenv_roles, :production
 
