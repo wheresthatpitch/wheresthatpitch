84323ea353e59cea0151dd7193735e9f2c824b99
Mike Griffin <mgriffin@github.com>
2023-04-03 15:41:27 +0100

    bind to a socket

--- config/puma.rb
+++ config/puma.rb
@@ -39,5 +39,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
+bind "unix:///var/www/wheresthatpitch/shared/tmp/sockets/puma.sock"
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
