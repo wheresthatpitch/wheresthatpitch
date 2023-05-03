542d1d882ed0df1d36706f98cfc942ca871a95af
Mike Griffin <mgriffin@github.com>
2023-05-03 22:04:47 +0100

    Add a task to run seeding on deploy
    Taken from
    https://blog.appsignal.com/2022/07/13/deploy-your-ruby-on-rails-app-using-capistrano.html

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,4 +62,15 @@ namespace :deploy do
       end
     end
   end
+
+  desc "Run seed"
+  task :seed do
+    on roles(:all) do
+      within current_path do
+        execute :bundle, :exec, 'rails', 'db:seed', 'RAILS_ENV=production'
+      end
+    end
+  end
+
+  after :migrating, :seed
 end
